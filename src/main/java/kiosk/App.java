a6e0e4e9ff5672d03130889fc0c8cb5a69fa0cab
MinHyeongK <syjjmh1@naver.com>
2025-07-18 18:32:47 +0900

    lv1

---
+++ src/main/java/kiosk/App.java
@@ -0,0 +1,27 @@
+package kiosk;
+
+import java.util.Scanner;
+
+public class App {
+    public static void main(String[] args) {
+        while(true) {
+            Scanner sc = new Scanner(System.in);
+            String[] menu = {"[ SHAKESHACK MENU ]",
+                "1. ShackBurger   | W 6.9 | 토마토, 양상추, 쉑소스가 토핑된 치즈버거",
+                "2. SmokeShack    | W 8.9 | 베이컨, 체리 페퍼에 쉑소스가 토핑된 치즈버거",
+                "3. Cheeseburger  | W 6.9 | 포테이토 번과 비프패티, 치즈가 토핑된 치즈버거",
+                "4. Hamburger     | W 5.4 | 비프패티를 기반으로 야채가 들어간 기본버거",
+                "0. 종료           | 종료"};
+
+            for (String i: menu){
+                System.out.println(i);
+            }
+
+            String exit = sc.next();
+            if (exit.equals("0")){
+                System.out.println("키오스크 종료");
+                break;
+            }
+        }
+    }
+}
--- src/main/java/lv1/kiosk.java
+++
@@ -1,7 +0,0 @@
-package lv1;
-
-public class kiosk {
-    public static void main(String[] args) {
-        System.out.println("git init");
-    }
-}
